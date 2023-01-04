da5f114e3b6ffc3ea2488a83ca7c091c4cb10eb4
DatSword <qbastard99@gmail.com>
2023-01-04 13:56:52 +0100

    non

--- Project1/Project1/chato_ext_cours_interieur.cs
+++ Project1/Project1/chato_ext_cours_interieur.cs
@@ -96,7 +96,6 @@ namespace SAE101
             _tiledMapRenderer.Update(gameTime);
 
             //Mouvement/animation
-            joueur.Update(gameTime);
             if (_stop == 1 && keyboardState.IsKeyUp(Keys.Down))
                 animation = "idle_down";
             else if (_stop == 2 && keyboardState.IsKeyUp(Keys.Up))
